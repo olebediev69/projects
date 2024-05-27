31edbd7604c2b7ff87a2db90a1a88ede36f2f854
olebediev69 <olebediev@kse.org.ua>
2024-05-27 15:39:15 +0300

    Tic-tac-toe game with colorama formatting, leaderboard, and metadata record.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,14 +3,20 @@
 from colorama import Fore,Back,Style
 import time
 
-# f = open('Users/oleksandrlebediev/PycharmProjects/projects/metadata.txt',w)
-#     f.write('Something')
-#     f.close()
+metadata = open('metadata.txt', 'w')
 
-def game_status(basic_matrix):
+def remove_colorama_formatting(text):
+    for color_code in [Fore.GREEN, Fore.RED, Fore.BLACK, Fore.CYAN, Fore.LIGHTCYAN_EX, Fore.BLUE, Style.RESET_ALL]:
+        text = text.replace(color_code, '')
+    return text
+
+def game_status(basic_matrix,metadata):
     basic_matrix_values = list(basic_matrix.values())
     for i in range(0, 9, 3):
-        print(basic_matrix_values[i], basic_matrix_values[i + 1], basic_matrix_values[i + 2])
+        row = f"{basic_matrix_values[i]} {basic_matrix_values[i + 1]} {basic_matrix_values[i + 2]}"
+        print(row)
+        metadata.write(remove_colorama_formatting(row) + '\n')
+    metadata.write('\n')
 
 def logic_block(basic_matrix,leaderboard):
     basic_matrix_values = list(basic_matrix.values())
@@ -34,10 +40,14 @@ def logic_block(basic_matrix,leaderboard):
             return 'O'
 
 leaderboard = {'X':0, 'O':0}
+game_number = 0
 
 while True:
     do_you_wanna_play = input(Fore.CYAN + "Do you wanna play (y/n):" + Style.RESET_ALL + ' ')
     if do_you_wanna_play.lower() == 'y':
+        game_number += 1
+        metadata.write(f'Game #{game_number} \n')
+        metadata.write('\n')
         start_time = time.time()
         basic_matrix = {
                         1: '•', 2: '•', 3: '•',
@@ -47,7 +57,7 @@ while True:
         previous_move = []
         print('Current game status is:')
         while '•' in basic_matrix.values():
-            game_status(basic_matrix)
+            game_status(basic_matrix,metadata)
             cell_character_input = input(Fore.BLACK + Back.WHITE + 'Enter a cell and a character, format: [cell_number-character]:' + Style.RESET_ALL + ' ')
             cell_input,xo_input = cell_character_input.split('-')
 
@@ -72,18 +82,24 @@ while True:
                 print(Fore.RED + 'The input values is wrong, try again' + Style.RESET_ALL)
             winner = logic_block(basic_matrix,leaderboard)
             if winner:
-                game_status(basic_matrix)
+                game_status(basic_matrix,metadata)
+                metadata.write(f'Game winner is {winner} \n')
                 print(Fore.LIGHTCYAN_EX + f'\n {winner} wins! \n' + Style.RESET_ALL)
                 print(Fore.BLUE + 'The current leaderboard is: ' + Style.RESET_ALL + Fore.GREEN + 'X' + Style.RESET_ALL + f':{leaderboard["X"]} ' + Fore.RED + 'O' + Style.RESET_ALL + f':{leaderboard["O"]}')
                 stop_time = time.time()
-                print(f'This game took {round(stop_time - start_time,0)} seconds')
+                metadata.write(f'This game took {round(stop_time - start_time,0)} seconds \n')
+                metadata.write('--- \n')
                 break
         else:
-            game_status(basic_matrix)
+            game_status(basic_matrix,metadata)
+            metadata.write('Draw \n')
+            metadata.write(f'This game took {round(stop_time - start_time, 0)} seconds \n')
+            metadata.write('--- \n')
             print('This is draw!')
             print(Fore.BLUE + 'The current leaderboard is: ' + Style.RESET_ALL + Fore.GREEN + 'X' + Style.RESET_ALL + f':{leaderboard["X"]} ' + Fore.RED + 'O' + Style.RESET_ALL + f':{leaderboard["O"]}')
     elif do_you_wanna_play.lower() == 'n':
         print('Thank you for playing!')
+        metadata.close()
         break
     else:
         print('Wrong input, please try again')
